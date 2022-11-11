7f4749aab1703c3d06c52e2b31ed9145d491e6ab
Ellen Cabral <ellencabral2098@gmail.com>
2022-11-11 01:42:49 -0300

    first commit

--- app/monitorAllData.js
+++ app/monitorAllData.js
@@ -1,8 +1,20 @@
-import  * as fb from "firebase/database";// sera preciso usar fb antes de cada funcao
+import * as fb from "firebase/database";// sera preciso usar fb antes de cada funcao
 import { dbConnect } from "./connetToFB.js";
 
 dbConnect()
 .then(db=>{//db contem a referencia ao banco
-    console.log(db)//mostra informacoes da conexao(pode excluir)
-    //implemente aqui
+    const refNode = fb.child(db,"produtos");
+
+    fb.get(refNode).then((snapshot)=>{
+        if(snapshot.exists()){
+            console.log(snapshot.val())
+        }else{
+            console.log("Nó não encontrado")
+        }
+        process.exit(0)
+    }).catch((erro)=>{
+        console.log(erro)
+        process.exit(0)
+    })
+
 }).catch(err=>console.log(err))
