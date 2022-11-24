f88f3243f1f8283427c41b3e2a57493cc3714214
Ellen <ellencabral2098@gmail.com>
2022-11-24 16:34:27 -0300

    feita tarefa 3

--- app/monitorAllData.js
+++ app/monitorAllData.js
@@ -4,6 +4,11 @@ import { dbConnect } from "./connetToFB.js";
 dbConnect()
 .then(db=>{//db contem a referencia ao banco
 
-    //implemente aqui
+    const refDB = fb.ref(db,'produtos');
+
+    fb.onValue(refDB,(snapshot) => {
+        console.log(snapshot.val())
+        process.exit(0)
+    })
 
 }).catch(err=>console.log(err))
