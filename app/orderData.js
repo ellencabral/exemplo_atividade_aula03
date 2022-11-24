fe1f93d8a959fad847244cf890b84ddc1190cf4e
Ellen <ellencabral2098@gmail.com>
2022-11-24 16:51:30 -0300

    feita tarefa 6

--- app/orderData.js
+++ app/orderData.js
@@ -3,6 +3,13 @@ import { dbConnect } from "./connetToFB.js";
 
 dbConnect()
 .then(db=>{//db contem a referencia ao banco
-    console.log(db)//mostra informacoes da conexao(pode excluir)
-    //implemente aqui
+    
+    const refDB = fb.ref(db,'produtos');
+
+    const consulta = fb.query(refDB,fb.orderByChild('descricao'))
+    
+    fb.onChildAdded(consulta,dados => {
+        console.log(dados.val())
+    })
+
 }).catch(err=>console.log(err))
