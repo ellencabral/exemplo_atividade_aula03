e519228f30d40f5730a05098d244be784406855b
Ellen <ellencabral2098@gmail.com>
2022-11-24 18:27:51 -0300

    feita tarefa 8

--- app/orderProdPrice.js
+++ app/orderProdPrice.js
@@ -3,6 +3,15 @@ import { dbConnect } from "./connetToFB.js";
 
 dbConnect()
 .then(db=>{//db contem a referencia ao banco
-    console.log(db)//mostra informacoes da conexao(pode excluir)
-    //implemente aqui
+    
+    const refDB = fb.ref(db);
+    const refNode = fb.child(refDB,"produtos");
+
+    const consulta = fb.query(refNode,fb.orderByChild('preco'))
+    
+    fb.onChildAdded(consulta,dados => {
+        console.log(dados.key)
+        console.log(dados.val())
+    })
+
 }).catch(err=>console.log(err))
